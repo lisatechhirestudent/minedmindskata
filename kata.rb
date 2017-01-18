e09ba584f98a5298d47f498d2129a7b72d7653a9
techhirestudent <live_work_achieve@outlook.com>
2017-01-18 10:41:28 -0500

    commiting

--- kata.rb
+++ kata.rb
@@ -1,7 +1,13 @@
-for current_iteration_number in 1..10 do
-puts "#{current_iteration_number}"	
+for k in 1..100 do
+	puts "Number #{k}"
 end
+
+
+
+#for current_iteration_number in 1..10 do
+#puts "#{current_iteration_number}"	
+#end
 # 
-for current_iteration_number in 1..100 do
-	puts "#{current_iteration_number}"
-end
+#for current_iteration_number in 1..100 do
+#	puts "#{current_iteration_number}"
+#end
---
+++ pizza.rb
@@ -0,0 +1,13 @@
+#(1..10).select(&:odd?)
+#end
+#
+puts 1
+puts 2
+puts 3
+puts 4
+puts "pizza"
+puts 6
+puts 7
+puts 8
+puts 9
+puts 10
