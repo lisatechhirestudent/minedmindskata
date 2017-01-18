b499dadf742e0db83778697f7e76f93d0311343f
techhirestudent <live_work_achieve@outlook.com>
2017-01-18 13:47:02 -0500

    current test passing

---
+++ test_minedmindskata.rb
@@ -0,0 +1,33 @@
+require "minitest/autorun"
+require_relative "theminedmindskata.rb"
+
+
+class TestMinedMindsKata < Minitest::Test
+
+    def test_1_returns_1
+    	assert_equal(1,1)
+	end
+
+	def test_function_returns_1
+		assert_equal(1, minedmindskata(1))
+	end
+
+	def test_function_returns_2
+		assert_equal(2,minedmindskata(2))
+	end
+
+	def test_function_returns_string_mined
+		assert_equal("mined", minedmindskata(3))
+	end						
+
+	def test_function_returns_5
+		assert_equal("minds",minedmindskata(5))
+	end
+
+	def test_function_returns_6
+	end		
+end
+
+
+
+
---
+++ theminedmindskata.rb
@@ -0,0 +1,10 @@
+
+def  minedmindskata(parameter1)
+	if parameter1 == 3
+		"mined"
+	elsif parameter1 ==5
+		"minds"
+	else
+		parameter1
+	end
+end
